0f036be647a86947ab80991aa8a216c25f8c1cd7
shaggyshelar <shaggy.shelar@gmail.com>
2020-05-03 18:02:36 +0530

    Implemented History of button click

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,5 +1,6 @@
 const intiaState = {
-    age : 21
+    age : 21,
+    history : []
   }
 
   const reducer = (state = intiaState , action) =>{
@@ -7,14 +8,22 @@ const intiaState = {
     const newState = {...state}
     if(action.type ===  'AGE_UP'){
       newState.age ++;
+      newState.history = newState.history.concat({id : Math.random(), value : newState.age })
     }
     if(action.type ===  'AGE_DOWN'){
       newState.age --;
+      newState.history = newState.history.concat({id : Math.random(), value : newState.age })
     }
 
+    if(action.type === 'DeleteItem'){
+      newState.history = newState.history.filter(el => el.id !== action.key)
+    }
+ 
 
     return newState
 
   }
 
   export default reducer;
+
+  
